850be4ba0e5b44c49213ba43f7e8dd01e5ad3fbd
rich p <richardferry3@gmail.com>
2022-05-29 12:43:18 -0700

    add about me section

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+function about(){
+    return(
+            <div>
+                <h2>My Name is Richard Ferry</h2>
+                <p>Full stack web developer with a certificate in full stack web development from  UC Berkeley Extension. 
+                    Bringing a business background, capable of creating an innovative user experience. With skills in HTML, CSS,  
+                    JavaScript, and node.js, as well as the ability to meet criteria, provide creative feedback, and work in 
+                    groups. Key knowledge in creating responsive mobile-first applications that are user friendly, utilized to 
+                    create a portfolio page, weather dashboard, and continuing to apply is current projects. Capable of 
+                    utilizing mysql for database development, and utilizing node.js for application use. In a recent project, 
+                    my role was to utilize Handlebars templating and javascript in order to design a responsive layout that is 
+                    user friendly. The project is also utilizing express,and express-session so that a user can have a personalized
+                    account to access certain content. Excited to utilize my skills in additional projects.</p>
+            </div>
+    )
+}
