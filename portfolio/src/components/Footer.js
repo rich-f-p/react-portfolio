77725a203951c8f1ae5d4f52538c8dc41f55104b
rich p <richardferry3@gmail.com>
2022-05-29 12:51:40 -0700

    add footer text content

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function footer(){
+    return (
+        <div>
+            <h3>Richard Ferry Portfolio ©</h3>
+            <a href="https://www.linkedin.com/in/richard-ferry-83120514b/">LinkedIn</a>
+            <a href="https://github.com/rich-f-p">Github</a>
+        </div>
+    );
+};
