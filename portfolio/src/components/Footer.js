4ee49359713b8b31646250741bbc171f39141357
rich p <richardferry3@gmail.com>
2022-06-01 04:36:16 -0700

    create footer that will have react icons, and links to github and linkedin

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import {AiFillGithub, AiFillLinkedin} from "react-icons/ai"
 
-function footer(){
+export default function footer(){
     return (
-        <div>
+        <footer className="footer mt-auto">
+            <div className="container-fluid d-flex justify-content-evenly">
             <h3>Richard Ferry Portfolio ©</h3>
-            <a href="https://www.linkedin.com/in/richard-ferry-83120514b/">LinkedIn</a>
-            <a href="https://github.com/rich-f-p">Github</a>
-        </div>
+            <a href="https://www.linkedin.com/in/richard-ferry-83120514b/"><AiFillLinkedin size={70} /></a>
+            <a href="https://github.com/rich-f-p"><AiFillGithub size={70} /></a>
+            </div>
+        </footer>
     );
 };
