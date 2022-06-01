acb18b6d33e43dc00a560ef5197f0a10ab9c4c46
rich p <richardferry3@gmail.com>
2022-06-01 10:21:42 -0700

    add background image

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Main from './components/Main';
+const background = {
+  bg:{
+  backgroundPosition: "center center",
+  backgroundAttachment: "fixed",
+  backgroundRepeat: "no-repeat", backgroundSize: "cover",
+  backgroundImage: `url("https://i.chzbgr.com/full/8490991872/hD7AFC2D1/make-it-rain")`,
+  }
+}
 
 function App() {
   return (
-    <div>
+    <div style={background.bg}>
       <Main />
     </div>
   );
