3cd31d5111a2ca0dc50a491db858f37de899450b
rich p <richardferry3@gmail.com>
2022-05-31 19:15:23 -0700

    add main.js to handle page change

---
+++ portfolio/src/components/Main.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import About from './About';
+import Contact from './Contact';
+import Footer from './Footer';
+import Projects from './Projects';
+import Resume from './Resume';
+
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Projects') {
+      return <Projects />;
+    }
+    if(currentPage === 'Resume'){
+        return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </div>
+  );
+}
