987e6b4e00f0dfd27a3b8f6839d44a9ee21a2483
rich p <richardferry3@gmail.com>
2022-06-01 23:10:20 -0700

    add third element/link to footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {AiFillGithub, AiFillLinkedin} from "react-icons/ai"
+import {SiMedium} from "react-icons/si"
 
 const text ={
     fontFamily: `'Playfair Display SC', serif`,
@@ -11,6 +12,7 @@ export default function footer(){
             <h3 style={text}>Richard Ferry Portfolio ©</h3>
             <a href="https://www.linkedin.com/in/richard-ferry-83120514b/"><AiFillLinkedin size={70} /></a>
             <a href="https://github.com/rich-f-p"><AiFillGithub size={70} /></a>
+            <a href="https://medium.com/@richardfpro864"><SiMedium size={70}></SiMedium></a>
             </div>
         </footer>
     );
