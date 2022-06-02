faa1f1e2f9e053ecaf01f8661d14a045812b1c8b
rich p <richardferry3@gmail.com>
2022-06-01 19:24:25 -0700

    add contact form functions

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import { validEmail } from "../utils/helpers";
 const sCon = {
     text:{
     color: "white",
@@ -12,24 +13,60 @@ const sCon = {
     }
 } 
 
-export default function contact(){
+export default function Contact(){
+
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [text,setText] = useState('');
+
+    const inputChange = (e) =>{
+        const {target} = e;
+        const inputType= target.name;
+        const inputVal = target.value;
+
+        if(inputType === 'name'){
+            setName(inputVal);
+        }else if (inputType === 'email'){
+            setEmail(inputVal);
+        } else {
+            setText(inputVal);
+        }
+    };
+
+    const submitForm = (e) =>{
+        e.preventDefault();
+        console.log(name);
+        console.log(email);
+
+        if(!validEmail(email)){
+            alert('invalid field');
+            return;
+        }else{
+            alert('Thank you!');
+            setEmail('');
+            setName('');
+            setText('');
+        };
+
+
+    }
+
     return (
         <div className="container mt-3 vh-100" style={sCon.bg}>
             <form>
                 <div className="form-group mt-3">
-                    <label for="exampleInputEmail1">Name</label>
-                    <input type="name" className="form-control" id="exampleInputEmail1"  placeholder="Enter Name" />
+                    <label>Name</label>
+                    <input value={name}className="form-control" name="name" type='text' onChange={inputChange}  placeholder="Enter Name" />
                 </div>
                 <div className="form-group mt-3">
-                    <label for="exampleInputPassword1">Email</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Email" />
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <label>Email</label>
+                    <input value={email} name='email' type="email" className="form-control" onChange={inputChange} placeholder="Email" />
                 </div>
-                <div class="form-group mt-3">
-                    <label for="exampleFormControlTextarea1">Example textarea</label>
-                    <textarea class="form-control mb-3" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <div className="form-group mt-3">
+                    <label>Please enter details about topic.</label>
+                    <input value={text} className="form-control mb-3" name='text' type='text' onChange={inputChange} rows="3"></input>
                 </div>
-                <button type="submit" class="btn btn-secondary">Submit</button>
+                <button type="button" className="btn btn-secondary" onClick={submitForm}>Submit</button>
             </form>
         </div>
     );
